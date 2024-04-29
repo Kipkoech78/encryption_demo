ed65c08ea565711d4e1613a869f8f0c6a9c5950c
Kipkoech78 <lkipkoech@kabarak.ac.ke>
2024-04-29 22:14:20 +0300

    second commit

--- app.js
+++ app.js
@@ -109,7 +109,12 @@ app.get("/auth/google/secrets",
     // Successful authentication, redirect secrets.
     res.redirect("/secrets");
   });
-
+app.get("/Contact", function(req, res){
+    res.render("Contact");
+});
+app.get("/index", function(req, res){
+    res.render("index");
+});
 
 app.get("/login", function(req, res){
     res.render("login");
@@ -203,6 +208,65 @@ app.post("/login", function(req,res){
     });
 
 });
+
+
+// newsletter
+
+
+
+app.post("/index",function(req, res){
+    const firstname= req.body.firstname;
+    const lastname= req.body.secondname;
+    const emailget= req.body.emailget;
+
+
+
+    const data ={
+        members:[
+            {
+                email_address:emailget,
+                status:"subscribed",
+                merge_fields:{
+                    FNAME:firstname,
+                    LNAME:lastname
+                }
+            }
+        ]
+    };
+    const jsonData = JSON.stringify(data);
+//read mdn documentation for more info on option on https  (nodejs.org)(https.request)
+
+    const url ="https://us22.api.mailchimp.com/3.0/lists/b0c58e0b7c";
+    const options ={
+        method:"POST",
+        auth: "kipkoech78:77e6ad8b82f435a1124c4768577e54a4-us22"
+
+    }
+    const request = https.request(url, options, function(response){
+
+        if (response.statusCode ===200){
+            res.sendFile(__dirname+"/success.html");
+        }
+        else{
+            res.sendFile(__dirname+"/failure.html");
+
+
+        };
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsonData);
+    request.end();
+
+});
+app.get("/failure",function(req,res){
+    res.redirect("/index");
+
+})
+
+
+
 
 
 
